ljacobsson/cfn-diagram

commit aa00e5687ce4a7e6f7e15489316ca24f10b45c9a

Fixed ciMode not writing draw.io diagrams to filesystem (#28)
Co-authored-by: Adrian Nieto Perez <anietoperez@sequel.com>

graph/MxGenerator.js
@@ -250,9 +250,11 @@ async function generate(cmd, template) {
   let resourceNames = { answer: resources };
   let edgeMode = { answer: "On" };
   let actionChoice = {};
-  console.log("Writing diagram to ./template.drawio");
+  console.log("Diagram will be written to " + cmd.outputFile);
 
   if (ciMode) {
+    const xml = renderTemplate(template);
+    fs.writeFileSync(cmd.outputFile, xml);
     return;
   }
 

commit 4bc588491a0ea9ba264a96b37fc5374a1809297e

Fixed ci-mode drawio diagram generation broken after #28

graph/MxGenerator.js
@@ -248,9 +248,14 @@ async function generate(cmd, template) {
   types = [...new Set(types)].sort();
   let resourceTypes = { answer: types };
   let resourceNames = { answer: resources };
-  let edgeMode = { answer: "On" };
+  let edgeMode = { answer: "On" }; 
+  filterConfig.resourceNamesToInclude = resourceNames.answer;
+  filterConfig.resourceTypesToInclude = resourceTypes.answer;
+  filterConfig.edgeMode = edgeMode.answer;
+
   let actionChoice = {};
   console.log("Diagram will be written to " + cmd.outputFile);
+
 
   if (ciMode) {
     const xml = renderTemplate(template);